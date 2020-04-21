4f2f30c2a397edf55f8903800f5f32b9f8bfd4ab
FproGeek <41802608+FproGeek@users.noreply.github.com>
2020-04-21 16:20:41 +0200

    Create fr.lua

---
+++ locales/fr.lua
@@ -0,0 +1,70 @@
+Locales['fr'] = {
+    -- Cloakroom
+    ['cloakroom']                = 'Vestiaire',
+    ['citizen_wear']             = 'Tenue civile',
+    ['brasseur_outfit']          = 'Tenue Brasseur',
+    ['no_outfit']                = 'Il n\'y a pas d\'uniforme à votre taille...',
+    ['open_cloackroom']          = 'Appuyez sur ~INPUT_CONTEXT~ pour vous changer',
+
+    -- Vault  
+    ['get_weapon']               = 'Prendre Arme',
+    ['put_weapon']               = 'Déposer Arme',
+    ['get_weapon_menu']          = 'Coffre - Prendre Arme',
+    ['put_weapon_menu']          = 'Coffre - Déposer Arme',
+    ['get_object']               = 'Prendre Objet',
+    ['put_object']               = 'Déposer Objet',
+    ['vault']                    = 'Coffre',
+    ['open_vault']               = 'Appuyez sur ~INPUT_CONTEXT~ pour accéder au coffre',
+
+    -- Vehicles  
+    ['vehicle_menu']             = 'Véhicule',
+    ['vehicle_out']              = 'Il y a déja un véhicule dehors',
+    ['vehicle_spawner']          = 'Appuyez sur ~INPUT_CONTEXT~ pour sortir un véhicule',
+    ['store_vehicle']            = 'Appuyez sur ~INPUT_CONTEXT~ pour ranger le véhicule',
+    ['service_max']              = 'Service complet : ',
+    ['spawn_point_busy']         = 'Un véhicule occupe le point de sortie',
+
+    -- Boss Menu  
+    ['take_company_money']       = 'Retirer argent société',
+    ['deposit_money']            = 'Déposer argent',
+    ['amount_of_withdrawal']     = 'Montant du retrait',
+    ['invalid_amount']           = 'Montant invalide',
+    ['amount_of_deposit']        = 'Montant du dépôt',
+    ['open_bossmenu']            = 'Appuyez sur ~INPUT_CONTEXT~ pour ouvrir le menu',
+    ['invalid_quantity']         = 'Quantité invalide',
+    ['you_removed']              = 'Vous avez retiré x',
+    ['you_added']                = 'Vous avez ajouté x',
+    ['quantity']                 = 'Quantité',
+    ['inventory']                = 'Inventaire',
+    ['brasseur_stock']            = 'Coffre de la brasserie',
+
+    -- Billing Menu  
+    ['billing']                  = 'Facture',
+    ['no_players_nearby']        = 'Aucun joueur à proximité',
+    ['billing_amount']           = 'Montant de la facture',
+    ['amount_invalid']           = 'Montant invalide',
+
+    -- Misc  
+    ['map_blip']                 = 'Brasserie',
+    ['brasseur']                  = 'Brasseur',
+
+    -- Phone  
+    ['brasseur_phone']            = 'Brasseur',
+    ['brasseur_customer']         = 'Citoyen',
+
+  ['brasseur_client'] = 'client brasseur',
+  ['transforming_in_progress'] = 'Transformation du houblon en cours',
+  ['houblon_taken'] = 'vous être en train de cultiver du houblon',
+  ['press_traitement'] = 'appuyez sur ~INPUT_CONTEXT~ pour transformer votre houblon',
+  ['press_collect'] = 'appuyez sur ~INPUT_CONTEXT~ pour cultiver du houblon',
+  ['press_brune'] = 'appuyez sur ~INPUT_CONTEXT~ pour transformer votre houblon en bière brune',
+  ['press_blonde'] = 'appuyez sur ~INPUT_CONTEXT~ pour transformer votre houblon en bière blonde',
+  ['press_sell'] = 'appuyez sur ~INPUT_CONTEXT~ pour vendre vos produits',
+  ['no_houblon_sale'] = 'vous n\'avez plus ou pas assez d\'houblon',
+  ['not_enough_houblon'] = 'vous n\'avez plus d\'houblon',
+  ['no_product_sale'] = 'vous n\'avez plus de produits',
+  ['not_enough_place'] = 'Vous n\'avez plus assez de place',
+  ['sale_in_prog'] = 'revente en cours...',
+  ['comp_earned'] = 'votre société a gagné ~g~$',
+  ['comp_earned_private'] = 'vous avez gagné ~g~$',
+}
